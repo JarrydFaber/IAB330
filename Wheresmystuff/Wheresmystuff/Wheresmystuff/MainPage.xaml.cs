7e40c12df77f61707bc8e5d17eedc0f80bbad640
JarrydFaber <jarrydbent@gmail.com>
2017-10-03 18:06:58 +1000

    Added home page and all other pages - buttons connect to each allocated page

---
+++ Wheresmystuff/Wheresmystuff/Wheresmystuff/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Wheresmystuff
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		void InventoryButton (object sender, EventArgs args)
+		{
+			// Write code here to move onto inventory page
+			Navigation.PushModalAsync(new Page1());
+		}
+
+		void BoxesButton(object sender, EventArgs args)
+		{
+			// Write code here to move onto inventory page
+			Navigation.PushModalAsync(new BoxPage());
+		}
+
+		void ScanButton(object sender, EventArgs args)
+		{
+			// Write code here to move onto inventory page
+			Navigation.PushModalAsync(new ScanPage());
+		}
+
+		void PrintButton(object sender, EventArgs args)
+		{
+			// Write code here to move onto inventory page
+			Navigation.PushModalAsync(new PrintPage());
+		}
+
+		private void inventoryButton_Clicked(object sender, EventArgs e)
+		{
+
+		}
+
+	}
+}
